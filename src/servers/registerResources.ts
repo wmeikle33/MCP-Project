625b398f8785fb6da14c31c111618782a7069801
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-28 10:33:24 +0800

    Update registerResources.ts

--- src/servers/registerResources.ts
+++ src/servers/registerResources.ts
@@ -1,1 +1,14 @@
+import { registerTools } from "./registerTools";
+import { registerResources } from "./registerResources";
 
+export function registerResources() {
+  const resource = new Resource(
+    { capabilities: { tools: {}, resources: {}, prompts: {} } }
+  );
+
+  registerTools(server);
+  registerResources(server);
+  registerPrompts(server);
+
+  return prompt;
+}
